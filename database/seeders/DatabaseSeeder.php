b0ef13db390fb9f092096a07bcbfaddd917fefac
Alexander Six <alexanderhsix@gmail.com>
2026-09-15 09:18:31 -0400

    feat: update seeders to add two distinct users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Alex Test',
+            'email' => 'alex@example.com',
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Leah Test',
+            'email' => 'leah@example.com',
         ]);
     }
 }
